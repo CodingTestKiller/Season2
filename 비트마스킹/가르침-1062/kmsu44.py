5dfe888733480d5110b5c753c00a88fabf580f8f
Minsu Kim <kmsu444@naver.com>
2023-06-27 14:13:30 +0900

    code수정

--- 비트마스킹/가르침-1062/kmsu44.py
+++ 비트마스킹/가르침-1062/kmsu44.py
@@ -6,32 +6,37 @@ for i in range(n):
     tmp = tmp[4:len(tmp)-4]
     words.append(tmp)
 
-T = set(['a', 'n', 't', 'c', 'i'])
-alphabet = set()
-k -= 5
-for word in words:
-    for j in word:
-        if j in T:
-            continue
-        alphabet.add(j)
-if k <= 0:
-    print(0)
-elif len(alphabet) < k:
-    print(n)
+
+if k >= 5:
+    T = set(['a', 'n', 't', 'c', 'i'])
+    alphabet = set()
+
+    for word in words:
+        for j in word:
+            if j in T:
+                continue
+            alphabet.add(j)
+    k -= 5
+    if len(alphabet) <= k:
+        print(n)
+    elif k < 0:
+        print(0)
+    else:
+        res = 0
+        L = list(combinations(alphabet, k))
+        for case in L:
+            cnt = 0
+            for word in words:
+                flag = True
+                for i in word:
+                    if i in case or i in T:
+                        continue
+                    else:
+                        flag = False
+                        break
+                if flag:
+                    cnt += 1
+            res = max(res, cnt)
+        print(res)
 else:
-    res = 0
-    L = list(combinations(alphabet, k))
-    for case in L:
-        cnt = 0
-        for word in words:
-            flag = True
-            for i in word:
-                if i in case or i in T:
-                    continue
-                else:
-                    flag = False
-                    break
-            if flag:
-                cnt += 1
-        res = max(res, cnt)
-    print(res)
+    print(0)
