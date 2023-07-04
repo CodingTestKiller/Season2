302ec6778995b0d369a44c7c73fb3725192b1718
Minsu Kim <kmsu444@naver.com>
2023-07-05 00:23:05 +0900

    chore : 코드 수정

--- 그리디/크게만들기-2812/kmsu44.py
+++ 그리디/크게만들기-2812/kmsu44.py
@@ -2,6 +2,7 @@ n, k = map(int, input().split())
 L = list(map(int, input()))
 
 S = []
+i = 0
 for i in range(n):
     if k < 0:
         break
@@ -10,5 +11,8 @@ for i in range(n):
         k -= 1
     S.append(L[i])
 
+while k > 0:
+    S.pop()
+    k -= 1
 for i in S:
     print(i, end='')
