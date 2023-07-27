f7950ad5ded62b5b74173abead5c05f0f3a43ff0
Minsu Kim <kmsu444@naver.com>
2023-07-27 15:49:26 +0900

    code 수정

--- DP/ACMCraft-1005/kmsu44.py
+++ DP/ACMCraft-1005/kmsu44.py
@@ -1,21 +1,4 @@
-# 1
-# 5 10
-# 100000 99999 99997 99994 99990
-# 4 5
-# 3 5
-# 3 4
-# 2 5
-# 2 4
-# 2 3
-# 1 5
-# 1 4
-# 1 3
-# 1 2
-# 4
-
-# 9시 10q분 시작
-from collections import defaultdict, deque
-import sys
+from collections import defaultdict
 
 T = int(input())
 result = []
@@ -39,24 +22,9 @@ for _ in range(T):
         a, b = map(int, input().split())
         delay_reverse[b].append(a)
 
-    start_list = []
-    for i in range(1, N+1):
-        if delay_reverse[i]:
-            continue
-        else:
-            start_list.append(i)
-
     W = int(input())
     dp = [-1 for _ in range(N+1)]
     findcost(W)
     result.append(dp[W])
 for i in result:
     print(i)
-
-# 1
-# 4 3
-# 1 1 1 1
-# 1 2
-# 3 2
-# 1 4
-# 4
