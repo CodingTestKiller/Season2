787eb38bdfd21bd326a0e2fa650b920685bab1f9
Minsu Kim <kmsu444@naver.com>
2023-08-01 16:46:30 +0900

    골목대장호석 / 3h+

---
+++ 최단거리/골목대장호석-20183/kmsu44.py
@@ -0,0 +1,54 @@
+import sys
+import heapq
+from collections import deque
+
+input = sys.stdin.readline
+n, m, a, b, c = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    s, e, w = map(int, input().split())
+    graph[s].append((e, w))
+    graph[e].append((s, w))
+
+res = sys.maxsize
+
+
+def dijkstra(mid):
+    q = []
+    heapq.heappush(q, (0, a))
+    distance = [sys.maxsize] * (n+1)
+    distance[a] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        # print('dist', dist, 'now', now)
+        if distance[now] < dist:
+            continue
+        for vertex, weight in graph[now]:
+            if weight > mid:
+                continue
+            if dist + weight < distance[vertex]:
+                distance[vertex] = dist + weight
+                heapq.heappush(q, (dist+weight, vertex))
+    if distance[b] > c:
+        return False
+    else:
+        return True
+
+
+l = 1
+r = 10 ** 14
+
+res = 0
+if not dijkstra(c):
+    print(-1)
+    exit()
+while l <= r:
+    mid = (l+r)//2
+    if dijkstra(mid):
+        res = mid
+        r = mid - 1
+    else:
+        l = mid + 1
+print(res)
